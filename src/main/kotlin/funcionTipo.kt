f2421a2a6df2a30f0ff8239c2180a025c8d4d427
Damian Nogueiras <damiannogueiras@gmail.com>
2021-05-04 06:48:43 +0200

    Definicion de tipo función

---
+++ src/main/kotlin/funcionTipo.kt
@@ -0,0 +1,21 @@
+class funcionTipo {
+    /**
+     * Declaracion Function Type
+     *
+     * AX: argumentos de la funcion
+     * R: tipo de dato que retorna
+     *
+     *   (A1,A2, ..., An) -> R
+     *
+     * Ejemplos:
+     *
+     *   (String, String) -> String
+     *   Dos String como argumento y devuelve String
+     *
+     *   (Init) -> Unit
+     *   Un Entero como argumento y devuleve nada en particular
+     *
+     *   () -> Double
+     *   No tiene parametros y devuelve un Double
+     */
+}
