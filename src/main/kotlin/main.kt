72a2741b852a74c7896c67d3ab5ef758f0ec450f
Damian Nogueiras <damiannogueiras@gmail.com>
2021-05-20 16:56:44 +0200

    Cambiamos lo que hace la función

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,16 +2,16 @@ fun main(args: Array<String>) {
 
     // Ejemplo pasar funcion por parámetro a otra función
 
-    // Definimos una función que recibe un entero y una string y 'hace algo'
+    // Definimos una función que recibe un entero y una string y 'hace algo diferente a lo anterior'
     /**
-     * Extrae una substring a partir de una cadena
-     * @param indice a partir se extrae la subcadena
-     * @param cadena que queremos extraer la subcadena
+     * Extrae un caracter de una cadena
+     * @param indice para extraer un contador
+     * @param cadena que queremos extraer un caracter
      * @return la subcadena extraida
      */
     fun utilizoEnteroSobreString( indice: Int, cadena: String): String {
-        // extraigo la subcadena a partir del indice pasado por parámetro
-        val nuevaCadena: String = cadena.substring(indice)
+        // quito el caracter del indice pasado por parámetro
+        val nuevaCadena: String = cadena.replace(cadena[indice].toString(),"")
         // devuelvo el valor
         return nuevaCadena
     }
@@ -19,9 +19,9 @@ fun main(args: Array<String>) {
     // Utilizamos esta funcion con dos ejemplos
 
     println(utilizoEnteroSobreString(2, "hola"))
-    // salida: la
+    // salida: hoa
 
     println(utilizoEnteroSobreString(5, "murcielago"))
-    // salida: elago
+    // salida: murcilago
 
 }
